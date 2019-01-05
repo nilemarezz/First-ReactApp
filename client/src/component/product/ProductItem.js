7524f4c71b64ba9d1aaf92b7b429f9f42e30c6c0
Unknown <nilenon@gmail.com>
2019-01-05 20:19:48 +0700

    [Add] Popup when confirm

--- client/src/component/product/ProductItem.js
+++ client/src/component/product/ProductItem.js
@@ -12,7 +12,7 @@ class ProductItem extends Component {
                 <div className="card-body">
                     <h5 className="card-title">{this.props.product.productName}</h5>
                     <p className="card-text">ราคา : {this.props.product.unitPrice}</p>
-                    <button type="button" class="btn btn-success btn-block" onClick={() => this.props.onAddOrder(this.props.product)}>ซื้อ</button>
+                    <button type="button" className="btn btn-success btn-block" onClick={() => this.props.onAddOrder(this.props.product)}>ซื้อ</button>
                     
                 </div>
             </div>
