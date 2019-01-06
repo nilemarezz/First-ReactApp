80cd5248de2503c4ff64a5c0a1483d821be98665
Unknown <nilenon@gmail.com>
2019-01-06 14:25:52 +0700

    [Edit] UI

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -1,46 +1,46 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {date : new Date()}
-        setInterval(() => this.tick(),1000)
-    }
-    
-    tick(){
-        this.setState({date : new Date()})
-    }
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
 
-    render() {
-        return (
-            <div >
-                <nav className="navbar navbar-expand-md bg-dark navbar-dark">
-                    
-                    <p className="navbar-brand" href="#"> <img style={{width:'50px'}} src='/images/logo/logo.png' alt='Logo'/>เฮลตี้ คาเฟ่ </p>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="collapsibleNavbar">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <p className="nav-link" >Link</p>
-                            </li>
-                            <li className="nav-item">
-                                <p className="nav-link" href="#">Link</p>
-                            </li>
-                            <li className="nav-item">
-                                <p className="nav-link" href="#">Link</p>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        )
-    } 
+  componentDidUpdate() {}
 
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
 
+  tick() {
+    // this.state = {date : new Date()};
+    this.setState({ date: new Date() });
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-8 text-left">
+            <h1 className="text-success">
+              <img style={{ height: 70 }} src="/images/logo/logo.png" alt="" />{" "}
+              เฮลตี้ คาเฟ่{" "}
+            </h1>
+          </div>
+          <div className="col-md-4 text-right">
+            <h5 className="text-muted mt-4">
+              {this.state.date.toLocaleTimeString()}
+            </h5>
+          </div>
+        </div>
+        <hr />
+      </div>
+    );
+  }
 }
 
 export default Header;
--- client/src/component/product/ProductItem.js
+++ client/src/component/product/ProductItem.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class ProductItem extends Component {
-    
-    
-    render() {
 
-        
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const {productName, unitPrice, thumbnail} = this.props.product;
         return (
-            <div className="card" style={{ width: '14rem'}}>
-                <img className="card-img-top" src={this.props.product.thumbnail} alt="CardImage" />
-                <div className="card-body">
-                    <h5 className="card-title">{this.props.product.productName}</h5>
-                    <p className="card-text">ราคา : {this.props.product.unitPrice}</p>
-                    <button type="button" className="btn btn-success btn-block" onClick={() => this.props.onAddOrder(this.props.product)}>ซื้อ</button>
-                    
-                </div>
+            <div className="col-md-3 col-sm-6">
+                <img className="img-fluid img-thumbnail" src={thumbnail} />
+                <h5 className="mt-2">{productName}</h5>
+                <p className="title text-right" >{unitPrice} THB</p>
+                <button className="btn btn-block btn-secondary title" onClick={() => this.props.onAddOrder(this.props.product)} >
+                    ซื้อ
+                </button>
+                <hr />
             </div>
         )
     }
 }
+
 export default ProductItem;
